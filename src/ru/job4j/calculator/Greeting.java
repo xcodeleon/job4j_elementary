5a4af1c8e72a2ccba95f097af9454b28597265e5
Eugene <xcodeleon@gmail.com>
2021-08-05 17:00:43 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/calculator/Greeting.java
@@ -0,0 +1,36 @@
+package ru.job4j.calculator;
+
+
+/*
+. Ниже приведен код класса ru.job4j.condition.Greeting.
+ 2. Внутри метода main выполните следующие действия:
+
+   - создайте переменную с именем idea типа String и присвойте ей значение "I like Java!";
+
+   - выведите на консоль эту переменную через команду System.out.println(idea);
+
+   - присоедините к переменной idea строку "But I am a newbie.";
+
+   - создайте переменную year с типом int и присвойте ей текущий год.
+
+   - присоедините к переменной idea переменную year;
+
+   - выведите на консоль эту переменную через команду System.out.println(idea);
+ */
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+
+        String str = "But I am a newbie.";
+        idea = idea + str;
+        System.out.println(idea);
+
+        int year = 2021;
+        idea = str + year;
+        System.out.println(idea);
+
+    }
+
+}
