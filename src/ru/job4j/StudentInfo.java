ab5b7e6ee0f0c2dd8b44f8d234f4a3bc2295c93f
Eugene <xcodeleon@gmail.com>
2021-08-05 17:10:00 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import javax.sound.midi.Soundbank;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Evgeniy");
+        System.out.println("01.02.1999");
+    }
+}
