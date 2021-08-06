bf49ba09b216b07f3ed706b95eaf50e0c5d8fa07
Eugene <xcodeleon@gmail.com>
2021-08-06 16:47:26 +0300

    0. Что такое тестирование [#159431]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,14 +2,10 @@ package ru.job4j.converter;
 
 /*
 Задание.
-
 1. Реализовать метод конвертации рублей в евро и доллары.
-
 Вам нужно заменить -1 на формулу для вычисления валюты.
-
 1 доллар = 60 рублей.
 1 евро = 70 рублей.
-
 2. Доработайте метод main. Добавьте в него примеры выводов всех видов конвертаций.
  */
 public class Converter {
@@ -24,9 +20,16 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(120);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("120 rubles are " + dollar + " dollar.");
+        float inEuro = 140;
+        float expectedEuro = 2;
+        float outEuro = Converter.rubleToEuro(inEuro);
+        boolean passedTestEuro = (expectedEuro == outEuro);
+        System.out.println("140 rubles are 2. Test result: " + passedTestEuro);
+
+        float inDollar = 180;
+        float expectedDollar = 3;
+        float outDollar = Converter.rubleToDollar(inDollar);
+        boolean passedTestDollar = (expectedDollar == outDollar);
+        System.out.println("180 rubles are 3. Test result: " + passedTestDollar);
     }
 }
